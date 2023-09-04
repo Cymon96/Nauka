bcc4187ffd971b02f7fc35c57279f9c39a188629
Cymon96 <143953435+Cymon96@users.noreply.github.com>
2023-09-04 22:40:37 +0200

    Update veterinary clinic.py
    Version 1.1

--- veterinary clinic.py
+++ veterinary clinic.py
@@ -1,7 +1,9 @@
 # Program do dodawania oraz usuwania pacjnetów i ich danych w placówce weterynaryjnej
 
 import time
+from enum import IntEnum
 
+Menu = IntEnum("Menu", "Register Unregister Show Search")
 Pets = {}
 
 while True:
@@ -10,47 +12,47 @@ while True:
     print("1 - Zarejestruj pacjenta\n2 - Wyrejestruj pacjenta\n3 - "
           "Wyświetl wszystkich zarejestrowanych pacjentów\n4 - Znajdź w bazie pacjenta")
 
-    Menu = input("Wybierz działanie: ")
+    choice = int(input("Wybierz działanie: "))
+    Choice = Menu(choice)
 
-    if Menu == "1":
-        print("\n")
-        Name = input("Podaj imie zwierzecia: ").capitalize()
-        Species = input("Podaj gatunek zwierzęcia: ").capitalize()
-        Age = input("Podaj wiek zwierzęcia: ") + " Lat"
-        Weight = input("Podaj wagę zwierzęcia: ") + "Kg"
-        OwnersName = input("Podaj imie właściciela: ").capitalize()
-        OwnersSurname = input("Podaj nazwisko właściciela: ").capitalize()
+    match Choice:
+        case Menu.Register:
+            print("\n")
+            Name = input("Podaj imie zwierzecia: ").capitalize()
+            Species = input("Podaj gatunek zwierzęcia: ").capitalize()
+            Age = input("Podaj wiek zwierzęcia: ") + " Lat"
+            Weight = input("Podaj wagę zwierzęcia: ") + "Kg"
+            OwnersName = input("Podaj imie właściciela: ").capitalize()
+            OwnersSurname = input("Podaj nazwisko właściciela: ").capitalize()
 
-        Pets[Name] = {
-            "Imie": Name,
-            "Gatunek": Species,
-            "Wiek": Age,
-            "Waga": Weight,
-            "Imie opiekuna": OwnersName,
-            "Nazwisko opiekuna": OwnersSurname
-        }
-        print("\n")
-        print(f"Zarejestrowano zwierzę o imieniu: {Name}")
-        for key, value in Pets[Name].items():
-            if key != "Imię":
-                print(f"{key}: {value}")
-    elif Menu == "2":
-        unregister = input("Podaj imie zwierzecia które chcesz wyrejestrować: ").capitalize()
-        if unregister in Pets:
-            del Pets[unregister]
-            print("Wyrejestrowano zwierze o imieniu: ", unregister)
-    elif Menu == "3":
-        for patient in Pets:
+            Pets[Name] = {
+                "Imie": Name,
+                "Gatunek": Species,
+                "Wiek": Age,
+                "Waga": Weight,
+                "Imie opiekuna": OwnersName,
+                "Nazwisko opiekuna": OwnersSurname
+            }
             print("\n")
-            for data in Pets[patient]:
-                print(data, Pets[patient][data])
-    elif Menu == "4":
-        search = input("Podaj imie zwierzęcia: ").capitalize()
-        if search in Pets:
-            print("\n")
-            for key, value in Pets[search].items():
-                if key != search:
+            print(f"Zarejestrowano zwierzę o imieniu: {Name}")
+            for key, value in Pets[Name].items():
                     print(f"{key}: {value}")
-    else:
-        print("Wybrałeś błędną pozycje. Spróbuj ponownie za 5 sekund")
-        time.sleep(5)
+        case Menu.Unregister:
+            unregister = input("Podaj imie zwierzecia które chcesz wyrejestrować: ").capitalize()
+            if unregister in Pets:
+                del Pets[unregister]
+                print("Wyrejestrowano zwierze o imieniu: ", unregister)
+        case Menu.Show:
+            for patient in Pets:
+                print("\n")
+                for data in Pets[patient]:
+                    print(data, Pets[patient][data])
+        case Menu.Search:
+            search = input("Podaj imie zwierzęcia: ").capitalize()
+            if search in Pets:
+                print("\n")
+                for key, value in Pets[search].items():
+                        print(f"{key}: {value}")
+        case _:
+            print("Wybrałeś błędną pozycję. Spróbuj ponownie za 5 sekund")
+            time.sleep(5)
